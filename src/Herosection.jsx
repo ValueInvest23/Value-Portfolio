8308b7ff5dfb3271c9c73fc5fe3838a297e7f70b
Prit1011 <pritgandhi0902@gmail.com>
2025-07-13 13:16:13 +0530

    changes in video background

--- src/Herosection.jsx
+++ src/Herosection.jsx
@@ -73,7 +73,7 @@ const HeroSection = () => {
         loop
         muted
         playsInline
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="absolute inset-0 w-full h-full object-cover z-0 bg-black"
       >
         <source src="/background1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
